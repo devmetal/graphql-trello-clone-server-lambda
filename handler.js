6d204562547bc510439a7745ff1ae52fe13fe57f
devmetal <metaladam91@gmail.com>
2019-02-17 23:01:58 +0100

    extend serverless config

--- handler.js
+++ handler.js
@@ -1,9 +1,15 @@
 const boardHandler = require('./src/schema/board/board.lambda');
 const ticketHandler = require('./src/schema/ticket/ticket.lambda');
+const commentHandler = require('./src/schema/comment/comment.lambda');
+const userHandler = require('./src/schema/user/user.lambda');
 
 module.exports.board = boardHandler;
 
 module.exports.ticket = ticketHandler;
+
+module.exports.comment = commentHandler;
+
+module.exports.user = userHandler;
 
 module.exports.hello = async event => ({
   statusCode: 200,
